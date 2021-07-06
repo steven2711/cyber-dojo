27f742843b3d1f37b0bd878363e51e92a29c5516
Steven Watson <swatson.emu@gmail.com>
2021-07-06 12:39:54 -0500

    Added imagesrcset for LCP. Test.

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,8 @@ class MyDocument extends Document {
           <link
             rel="preload"
             as="image"
-            href="/images/people-working-on-computers.jpg"
+            href="/images/people-working-on-computers-1920.jpg"
+            imagessrcset="/images/people-working-on-computers-1920.jpg 1920w, /images/people-working-on-computers-1024.jpg 1024w, /images/people-working-on-computers-540.jpg 540w"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
