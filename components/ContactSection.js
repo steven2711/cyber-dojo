ab304607fdb3f0f989761c559d1f5c37ae4ac7c1
Steven Watson <swatson.emu@gmail.com>
2021-07-19 13:18:17 -0500

    Apostrophy build error

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -4,7 +4,7 @@ import Button from "../components/Button";
 export default function ContactSection() {
   return (
     <section className={styles.section}>
-      <h2>let's talk business</h2>
+      <h2>let&apos;s talk business</h2>
       <p>Send us a message to see if our sevices are right for you!</p>
       <Button text="contact us" link={true} path="/contact" />
     </section>
